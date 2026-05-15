c32565b0fa22ef23576c84073ee255919f4d2a69
DiogoLpes <diogolopes285@gmail.com>
2026-05-15 19:04:22 +0000

    feat/bug detector setup of the agent

--- srcs/backend/main.py
+++ srcs/backend/main.py
@@ -2,7 +2,7 @@
 from agents_ai.agents.tasks import build_bug_task
 
 def start_review():
-    my_bundle = build_bug_task(repo_url, arquivos_lidos)
+    my_bundle = build_bug_task(repo_url, readed_files, modified_files, deleted_files)
 
     inputs = {
         'repo_url': 'https://github.com/Huniity/multigent.git',
