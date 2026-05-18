d15e6760eebb65301345b78970b07c015c42887e
ETIC <>
2026-05-18 21:31:46 +0100

    add basic auth pages (WIP)

--- srcs/frontend/src/main.tsx
+++ srcs/frontend/src/main.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD:srcs/frontend/src/main.tsx
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
@@ -8,3 +9,18 @@ createRoot(document.getElementById('root')!).render(
     <App />
   </StrictMode>,
 )
+=======
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import './index.css'
+import App from './App.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>,
+)
+>>>>>>> 5de068f (add basic auth pages (WIP)):frontend/src/main.tsx
