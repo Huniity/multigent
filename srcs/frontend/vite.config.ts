4171a5119870bc4299acefde5fd8d3758607a7b9
Huniity <adrien.dejonc@gmail.com>
2026-05-20 16:52:54 +0000

    Refactor frontend: Update Vite configuration, remove main.jsx, and integrate Tailwind CSS
    - Replaced the existing Vite SVG logo with an updated version.
    - Removed the old index.css styles and integrated Tailwind CSS.
    - Deleted main.jsx and transitioned to main.tsx for TypeScript support.
    - Added vite-env.d.ts for Vite client types.
    - Updated TypeScript configuration files for better compatibility and structure.
    - Enhanced Vite configuration to include Tailwind CSS plugin and set up server proxy for API calls.

--- srcs/frontend/vite.config.ts
+++ srcs/frontend/vite.config.ts
@@ -1,14 +1,16 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
-import tailwindcss from '@tailwindcss/vite'
+/// <reference types="vite/client" />
+
+// import { defineConfig } from 'vite'
+// import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+// import babel from '@rolldown/plugin-babel'
+// import tailwindcss from '@tailwindcss/vite'
 
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] }),
-    tailwindcss()
-  ],
-})
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     babel({ presets: [reactCompilerPreset()] }),
+//     tailwindcss()
+//   ],
+// })
